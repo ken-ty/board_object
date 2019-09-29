9a72abd6e7c646bf83423687fab5f14d5a443ef6
kentokura <healthy1109@gmail.com>
2019-09-29 12:48:42 +0900

    fix produced by gon

---
+++ test.php
@@ -0,0 +1,9 @@
+<?php
+require_once __DIR__ .
+    'libs/smarty.class.php';
+$fruit = 'リンゴリラ';
+$smarty = new Smarty();
+$smarty->setTemplateDir(__DIR__ . 'templates');
+$smarty->setCompileDir(__DIR__ . 'templates_c');
+$smarty->assign('fruit', $fruit);
+$smarty->display('test.tpl');
